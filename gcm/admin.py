634c10cec96fd2192f32bd074f235a233bef0786
ruckit-dev <46458158+ruckit-dev@users.noreply.github.com>
2020-10-07 13:47:04 -0500

    switched to render from render_to_response

--- gcm/admin.py
+++ gcm/admin.py
@@ -1,6 +1,6 @@
 from functools import update_wrapper
 from django.contrib import admin
-from django.shortcuts import redirect, render_to_response
+from django.shortcuts import redirect, render
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 
@@ -55,8 +55,7 @@ class DeviceAdmin(admin.ModelAdmin):
             return redirect(base_view)
 
         context = {'form': form, 'opts': self.model._meta, 'add': False}
-        return render_to_response('gcm/admin/send_message.html', context,
-                                  context_instance=RequestContext(request))
+        return render(request=request, template_name='gcm/admin/send_message.html', context=context)
 
     def send_message_action(self, request, queryset):
         ids = queryset.values_list('id', flat=True)
